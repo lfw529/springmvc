f04354e6b19a6a3fe223624a7671fcf03e5b0fc0
lfw529 <differentlee1012@163.com>
2022-07-04 15:27:38 +0800

    111

---
+++ 11_page/src/main/java/com/lfw/exception/BusinessException.java
@@ -0,0 +1,19 @@
+package com.lfw.exception;
+
+//自定义异常处理器，用于封装异常信息，对异常进行分类
+public class BusinessException extends RuntimeException {
+    private Integer code;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public BusinessException(Integer code, String message) {
+        super(message);
+        this.code = code;
+    }
+}
